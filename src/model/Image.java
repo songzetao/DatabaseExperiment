107837d325c36915f9537f91d3ba6fcdbcacc26c
wangxdouc <wangxiaodong@ouc.edu.cn>
2020-12-11 17:37:38 +0800

    init web app

---
+++ src/model/Image.java
@@ -0,0 +1,37 @@
+package model;
+
+public class Image {
+	private String title;
+	private String description;
+	private String path;
+	
+	public Image() {
+		
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+	
+}
