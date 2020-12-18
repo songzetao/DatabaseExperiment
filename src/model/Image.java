45e4b753d8988e3b5071d6fe6c42afb90d65af94
wangxdouc <wangxiaodong@ouc.edu.cn>
2020-12-18 16:24:00 +0800

    20201218

--- src/model/Image.java
+++ src/model/Image.java
@@ -1,12 +1,28 @@
 package model;
 
+import java.util.Date;
+
+import com.alibaba.fastjson.annotation.JSONField;
+
 public class Image {
+	private int no;
 	private String title;
 	private String description;
+	@JSONField(name = "src")
 	private String path;
-	
+	@JSONField(format = "yyyy-MM-dd HH:mm:ss")
+	private Date timestamp;
+
 	public Image() {
-		
+
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
 	}
 
 	public String getTitle() {
@@ -32,6 +48,18 @@ public class Image {
 	public void setPath(String path) {
 		this.path = path;
 	}
-	
-	
+
+	@Override
+	public String toString() {
+		return "{\"title\": \"" + title + "\", \"description\": \"" + description + "\", \"path\": \"" + path + "\"}";
+	}
+
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+
 }
